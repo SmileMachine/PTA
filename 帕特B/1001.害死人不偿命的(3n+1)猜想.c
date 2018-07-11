41996077da46c91ed41a087d2feca1ff818a63c1
SmileMachine <34188812+SmileMachine@users.noreply.github.com>
2018-07-11 10:54:45 +0800

    Create 1001.害死人不偿命的(3n+1)猜想.c

---
+++ 帕特B/1001.害死人不偿命的(3n+1)猜想.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	int cnt=0;
+	int n;
+	scanf("%d",&n);
+	for(cnt=0;n!=1;cnt++){
+		n=(n%2==0)?(n/2):((3*n+1)/2);
+	}
+	printf("%d",cnt);
+	return 0;
+} 
